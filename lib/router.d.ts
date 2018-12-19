37573c24309e7178cb1d895c48530dad8c38f2a0
Artyom Antyipin <artyom@skape.io>
2018-12-19 11:47:06 +0100

    router fix.

--- lib/router.d.ts
+++ lib/router.d.ts
@@ -1,7 +1,28 @@
+import { PolymerElement } from '@polymer/polymer/polymer-element';
 export interface PageData {
     component: string;
     arguments: any;
 }
+export interface StringMap {
+    [key: string]: string;
+}
+interface DopeRouter {
+    notFound: boolean;
+    currentPage: PageData;
+    defaultComponent: string;
+    defaultArguments?: StringMap;
+    initPage(page: PageData, isPop?: boolean, replace?: boolean): void;
+    _currentPageChanged(page: PageData): void;
+    _resolveComponentPath(component: string): string;
+    _loadComponent(component: string): Promise<any>;
+}
+declare namespace DopeRouter {
+    function attachLinkHandlers(root: Node): void;
+}
+interface Ctor<T> {
+    new (...args: any[]): T;
+}
+export declare const DopeRouterMixin: <T extends PolymerElement>(base: Ctor<T>) => Ctor<T & DopeRouter>;
 export declare const DopeGotoMixin: (base: any) => {
     new (): {
         [x: string]: any;
@@ -9,3 +30,4 @@ export declare const DopeGotoMixin: (base: any) => {
     };
     [x: string]: any;
 };
+export {};
--- src/router.ts
+++ src/router.ts
@@ -2,153 +2,183 @@ import { dedupingMixin } from '@polymer/polymer/lib/utils/mixin';
 import { property } from '@polymer/decorators/lib/decorators';
 import { Uri } from 'dopees-core/lib/uri';
 import { fromBase64String } from 'dopees-core/lib/string';
+import { PolymerElement } from '@polymer/polymer/polymer-element';
 
 export interface PageData {
-  component: string,
-  arguments: any
+  component: string;
+  arguments: any;
+}
+
+interface PageDataArgs {
+  page: PageData;
+  replace?: boolean;
 }
 
 interface NamedPromiseMap {
   [key: string]: Promise<any>;
 }
 
-const DopeRouterMixin = Object.assign(
-  dedupingMixin(base => {
-    abstract class DopeRouted extends base {
-      __pageComponents: NamedPromiseMap = {};
+export interface StringMap {
+  [key: string]: string;
+}
 
-      @property({ type: Boolean, notify: true, reflectToAttribute: true })
-      notFound!: boolean;
+interface DopeRouter {
+  notFound: boolean;
+  currentPage: PageData;
+  defaultComponent: string;
+  defaultArguments?: StringMap;
+  initPage(page: PageData, isPop?: boolean, replace?: boolean): void;
+  _currentPageChanged(page: PageData): void;
+  _resolveComponentPath(component: string): string;
+  _loadComponent (component: string): Promise<any>;
+}
 
-      @property({ type: Object, notify: true, observer: '_currentPageChanged' })
-      currentPage!: PageData;
-
-      get defaultComponent () { return 'admin-dashboard'; }
-
-      get defaultArguments () { return undefined; }
-
-      ready () {
-          super.ready();
-          // vissza gomb lekezelése
-          window.addEventListener('popstate', e => {
-              if (e.state) {
-                  this.initPage(e.state, true);
-              }
-          }, false);
-          // kezdei állapot kilvasása url-ből
-          const uri = Uri.from(window.location.href);
-          const initialPage = {
-              component: (uri.path.substring(1).replace('/', '-') || this.defaultComponent),
-              arguments: (uri.path.substring(1).replace('/', '-') ? uri.queryParams : this.defaultArguments)
-          };
-          // külső layout-ban lévő linkek lekezelése.
-          DopeRouterMixin.attachLinkHandlers(this.shadowRoot);
-          // oldalváltás lekezelése.
-          this.shadowRoot.addEventListener('goto-page', e => {
-              e.preventDefault();
-              e.stopPropagation();
-              this.initPage(e.detail.page, false, !!e.detail.replace);
-          }, true);
-          this.initPage(initialPage, false, true);
+namespace DopeRouter {
+  export function attachLinkHandlers(root: Node) {
+    root.addEventListener('click', (ex: Event) => {
+      const e = <MouseEvent>ex;
+      if (e.shiftKey || e.ctrlKey || e.metaKey) {
+        return;
       }
-      initPage (page: PageData, isPop?: boolean, replace?: boolean) {
-          if (!page) {
-              throw new Error('page not specified in initPage.');
+      if (e.target instanceof Element) {
+        let target: Element|undefined|null = e.target;
+        while (target && target.tagName !== 'A' && !(target.getAttribute && target.getAttribute('data-page'))) {
+          if (target.hasAttribute && target.hasAttribute('suppress-goto')) {
+            return;
           }
-          let confirmLeave = Promise.resolve();
-          if (this.__previousPage && this.__previousPage.beforeLeave) {
-              confirmLeave = Promise.resolve(this.__previousPage.beforeLeave()); // dope.confirm('Az aktuális oldal tartalmaz nem mentett változást...', 'Biztosan vált oldalt?');
-          }
-          confirmLeave.then(() => {
-              return this._loadComponent(page.component)
-                  .then(null, () => this.notFound = true)
-                  .then(() => {
-                      this.notFound = false;
-                      if (!isPop) {
-                          const uri = new Uri('');
-                          uri.path = '/' + page.component.replace('-', '/');
-                          uri.queryParams = page.arguments || {};
-                          if (replace) {
-                              window.history.replaceState(page, '', uri.href);
-                          } else {
-                              window.history.pushState(page, '', uri.href);
-                          }
-                      }
-                      this.currentPage = page;
-                  });
-          }, () => {});
-
-      }
-      _currentPageChanged (page: PageData) {
-          if (!this.notFound) {
-              // wait for dom-if to apply changes
-              setTimeout(() => {
-                  if (this.__previousPage) {
-                      this.__previousPage.leave();
-                  }
-                  const pageElement = this.shadowRoot.querySelector(page.component);
-                  if (pageElement) {
-                      this.__previousPage = pageElement;
-                      pageElement.trigger('init', { detail : this.currentPage.arguments || { } });
-                  } else {
-                      this.notFound = true;
-                  }
-              }, 0);
-          }
-      }
-      abstract _resolveComponentPath(component: string): string;
-      _loadComponent (component: string) {
-        if (!this.__pageComponents[component]) {
-          this.__pageComponents[component] = import(this._resolveComponentPath(component)).catch(() => null);
+          target = target.parentNode instanceof Element ? target.parentNode : null;
         }
-        return this.__pageComponents[component];
-      }
-    };
-    return DopeRouted;
-  }), {
-    attachLinkHandlers: root => {
-      root.addEventListener('click', (e: MouseEvent) => {
-          if (e.shiftKey || e.ctrlKey || e.metaKey) {
-              return;
+        if (!target) {
+          return;
+        }
+        if (target.hasAttribute && (target.hasAttribute('suppress-goto') || target.hasAttribute('draggable'))) {
+          return;
+        }
+        const rawPage = target.getAttribute('data-page');
+        if (!rawPage) {
+          return;
+        }
+        e.preventDefault();
+        e.stopPropagation();
+        const page = <PageData>JSON.parse(fromBase64String(rawPage));
+        target.dispatchEvent(new CustomEvent('goto-page', {
+          bubbles: true,
+          composed: true,
+          detail: {
+            page,
+            source: target
           }
-          if (e.target instanceof Element) {
-            let target: Element|undefined|null = e.target;
-            while (target && target.tagName !== 'A' && !(target.getAttribute && target.getAttribute('data-page'))) {
-                if (target.hasAttribute && target.hasAttribute('suppress-goto')) {
-                    return;
-                }
-                target = target.parentNode instanceof Element ? target.parentNode : null;
+        }));
+      }
+    }, true)
+  }
+}
+
+interface Ctor<T> {
+  new (...args: any[]): T;
+}
+
+export const DopeRouterMixin = dedupingMixin(<T extends PolymerElement>(base: Ctor<T>) => {
+  abstract class DopeRouted extends (<Ctor<PolymerElement>>base) {
+    __pageComponents: NamedPromiseMap = {};
+    __previousPage: any; //FIXME: typing
+
+    @property({ type: Boolean, notify: true, reflectToAttribute: true })
+    notFound!: boolean;
+
+    @property({ type: Object, notify: true, observer: '_currentPageChanged' })
+    currentPage!: PageData;
+
+    get defaultComponent () { return 'admin-dashboard'; }
+
+    get defaultArguments () { return undefined; }
+
+    ready () {
+        super.ready();
+        // vissza gomb lekezelése
+        window.addEventListener('popstate', e => {
+            if (e.state) {
+                this.initPage(e.state, true);
             }
-            if (!target) {
-                return;
-            }
-            if (target.hasAttribute && (target.hasAttribute('suppress-goto') || target.hasAttribute('draggable'))) {
-                return;
-            }
-            const rawPage = target.getAttribute('data-page');
-            if (!rawPage) {
-                return;
-            }
+        }, false);
+        // kezdei állapot kilvasása url-ből
+        const uri = Uri.from(window.location.href);
+        const initialPage = {
+            component: (uri.path.substring(1).replace('/', '-') || this.defaultComponent),
+            arguments: (uri.path.substring(1).replace('/', '-') ? uri.queryParams : this.defaultArguments)
+        };
+        // külső layout-ban lévő linkek lekezelése.
+        if (this.shadowRoot) {
+          DopeRouter.attachLinkHandlers(this.shadowRoot);
+          // oldalváltás lekezelése.
+          this.shadowRoot.addEventListener('goto-page', ex => {
+            const e = <CustomEvent<PageDataArgs>>ex;
             e.preventDefault();
             e.stopPropagation();
-            const page = <PageData>JSON.parse(fromBase64String(rawPage));
-            target.dispatchEvent(new CustomEvent('goto-page', {
-                bubbles: true,
-                composed: true,
-                detail: {
-                    page,
-                    source: target
-                }
-            }));
-          }
-      }, true)
+            this.initPage(e.detail.page, false, !!e.detail.replace);
+          }, true);
+        }
+        this.initPage(initialPage, false, true);
     }
-  }
-);
+    initPage (page: PageData, isPop?: boolean, replace?: boolean) {
+        if (!page) {
+            throw new Error('page not specified in initPage.');
+        }
+        let confirmLeave = Promise.resolve();
+        if (this.__previousPage && this.__previousPage.beforeLeave) {
+            confirmLeave = Promise.resolve(this.__previousPage.beforeLeave()); // dope.confirm('Az aktuális oldal tartalmaz nem mentett változást...', 'Biztosan vált oldalt?');
+        }
+        confirmLeave.then(() => {
+            return this._loadComponent(page.component)
+                .then(null, () => this.notFound = true)
+                .then(() => {
+                    this.notFound = false;
+                    if (!isPop) {
+                        const uri = new Uri('');
+                        uri.path = '/' + page.component.replace('-', '/');
+                        uri.queryParams = page.arguments || {};
+                        if (replace) {
+                            window.history.replaceState(page, '', uri.href);
+                        } else {
+                            window.history.pushState(page, '', uri.href);
+                        }
+                    }
+                    this.currentPage = page;
+                });
+        }, () => {});
+
+    }
+    _currentPageChanged (page: PageData) {
+      if (!this.notFound) {
+        // wait for dom-if to apply changes
+        setTimeout(() => {
+          if (this.__previousPage) {
+            this.__previousPage.leave();
+          }
+          const pageElement = this.shadowRoot && this.shadowRoot.querySelector(page.component);
+          if (pageElement) {
+            this.__previousPage = pageElement;
+            pageElement.dispatchEvent(new CustomEvent('init', { detail : this.currentPage.arguments || { } }));
+          } else {
+            this.notFound = true;
+          }
+        }, 0);
+      }
+    }
+    abstract _resolveComponentPath(component: string): string;
+    _loadComponent (component: string) {
+      if (!this.__pageComponents[component]) {
+        this.__pageComponents[component] = import(this._resolveComponentPath(component)).catch(() => null);
+      }
+      return this.__pageComponents[component];
+    }
+  };
+  return <Ctor<T & DopeRouter>><unknown>DopeRouted;
+});
 
 export const DopeGotoMixin = dedupingMixin(base => class extends base {
   ready () {
       super.ready();
-      DopeRouterMixin.attachLinkHandlers(this.shadowRoot);
+      DopeRouter.attachLinkHandlers(this.shadowRoot);
   }
 });
