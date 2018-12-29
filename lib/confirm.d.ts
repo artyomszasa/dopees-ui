d0964a88ae9e277635863e3aa7090c1b7fac9e97
Artyom Antyipin <artyom@skape.io>
2018-12-29 19:04:18 +0100

    dialog fixes, confirm dialog.

---
+++ lib/confirm.d.ts
@@ -0,0 +1,33 @@
+import './dialog/dialog';
+import { PolymerElement } from '@polymer/polymer/polymer-element';
+export interface DopeConfirmInit {
+    caption?: string;
+    confirmButtonLabel?: string;
+    cancelButtonLabel?: string;
+}
+/**
+ * Implments asynchronous confirmation dialog.
+ */
+export declare class DopeConfirm extends PolymerElement {
+    static readonly template: HTMLTemplateElement;
+    /**
+     * Shows confirmation dialog, returns promise that is resolved if user has clicked the confirm button, or rejected if
+     * user has clicked the cancel button.
+     *
+     * @param message Confirmation message to show.
+     * @param caption Optional alert caption.
+     */
+    static show(message: string, caption?: DopeConfirmInit | string): Promise<void>;
+    /**
+     * Caption of the dialog.
+     */
+    caption?: string;
+    /**
+     * Alert message.
+     */
+    message?: string;
+    confirmButtonLabel: string;
+    cancelButtonLabel: string;
+    onOkClick(): void;
+    onCancelClick(): void;
+}
---
+++ src/confirm.ts
@@ -0,0 +1,84 @@
+import './dialog/dialog';
+import { PolymerElement } from '@polymer/polymer/polymer-element';
+import { mkTemplate } from './templates';
+import { property, customElement } from '@polymer/decorators/lib/decorators';
+import dom from './dom';
+import view from './confirm/confirm.pug';
+
+export interface DopeConfirmInit {
+  caption?: string;
+  confirmButtonLabel?: string,
+  cancelButtonLabel?: string
+}
+
+/**
+ * Implments asynchronous confirmation dialog.
+ */
+@customElement('dope-confirm')
+export class DopeConfirm extends PolymerElement {
+  static get template() { return mkTemplate(view); }
+
+  /**
+   * Shows confirmation dialog, returns promise that is resolved if user has clicked the confirm button, or rejected if
+   * user has clicked the cancel button.
+   *
+   * @param message Confirmation message to show.
+   * @param caption Optional alert caption.
+   */
+  static show(message: string, caption?: DopeConfirmInit|string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const dialog = <DopeConfirm>document.createElement('dope-confirm');
+      dom.body.appendChild(dialog);
+      dialog.message = message;
+      if (caption) {
+        if ('string' === typeof caption) {
+          dialog.caption = caption;
+        } else {
+          dialog.caption = caption.caption || 'Megerősítés';
+          if (caption.confirmButtonLabel) {
+            dialog.confirmButtonLabel = caption.confirmButtonLabel;
+          }
+          if (caption.cancelButtonLabel) {
+            dialog.cancelButtonLabel = caption.cancelButtonLabel;
+          }
+        }
+      } else {
+        dialog.caption = 'Megerősítés';
+      }
+      dialog.addEventListener('dope-ok', () => {
+        dialog.remove();
+        resolve();
+      }, false);
+      dialog.addEventListener('dope-ok', () => {
+        dialog.remove();
+        reject('cancelled');
+      }, false)
+    });
+  }
+
+  /**
+   * Caption of the dialog.
+   */
+  @property({ type: String })
+  caption?: string
+
+  /**
+   * Alert message.
+   */
+  @property({ type: String })
+  message?: string
+
+  @property({ type: String })
+  confirmButtonLabel: string = 'Megerősítés';
+
+  @property({ type: String })
+  cancelButtonLabel: string = 'Mégsem';
+
+  onOkClick() {
+    this.dispatchEvent(new CustomEvent('dope-ok'));
+  }
+
+  onCancelClick() {
+    this.dispatchEvent(new CustomEvent('dope-cancel'));
+  }
+}
